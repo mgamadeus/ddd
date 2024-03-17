4a06b4dc16a89348336f8c6d0bae560df9ff895c
Marius-Eugen Gerdan <marius@gerdan.net>
2024-03-17 21:14:54 +0100

    Moved findByEmail from DBAccount to AccountsService

--- src/Domain/Common/Repo/DB/Accounts/DBAccount.php
+++ src/Domain/Common/Repo/DB/Accounts/DBAccount.php
@@ -9,10 +9,6 @@ use DDD\Domain\Base\Repo\DB\DBEntity;
 use DDD\Domain\Base\Repo\DB\Doctrine\DoctrineModel;
 use DDD\Domain\Base\Repo\DB\Doctrine\DoctrineQueryBuilder;
 use DDD\Domain\Common\Entities\Accounts\Account;
-use DDD\Infrastructure\Exceptions\BadRequestException;
-use DDD\Infrastructure\Exceptions\InternalErrorException;
-use Psr\Cache\InvalidArgumentException;
-use ReflectionException;
 
 /**
  * @method Account find(DoctrineQueryBuilder|string|int $idOrQueryBuilder, bool $useEntityRegistryCache = true, ?DoctrineModel &$loadedOrmInstance = null, bool $deferredCaching = false)
@@ -23,20 +19,4 @@ class DBAccount extends DBEntity
 {
     public const BASE_ENTITY_CLASS = Account::class;
     public const BASE_ORM_MODEL = DBAccount::class;
-
-    /**
-     * @param string $email
-     * @return Account|null
-     * @throws BadRequestException
-     * @throws InternalErrorException
-     * @throws InvalidArgumentException
-     * @throws ReflectionException
-     */
-    public function findByEmail(string $email): ?Account
-    {
-        $queryBuilder = static::createQueryBuilder();
-        $alias = self::getBaseModelAlias();
-        $queryBuilder->where("{$alias}.email = :email")->setParameter('email', $email);
-        return $this->find($queryBuilder);
-    }
 }
