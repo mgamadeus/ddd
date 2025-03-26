9aec279136c07d652da730dcc8880d5e4c75809c
Marius-Eugen Gerdan <marius@gerdan.net>
2025-03-26 13:10:38 +0100

    Update version and add getter for PaginationCursor
    Bump the package version to 2.5.23 in `composer.json`. Introduce a `getPaginationCursor` method in `PaginationCursorTrait` to ensure the cursor is instantiated when accessed.

--- src/Domain/Common/Entities/Pagination/PaginationCursorTrait.php
+++ src/Domain/Common/Entities/Pagination/PaginationCursorTrait.php
@@ -7,4 +7,12 @@ namespace DDD\Domain\Common\Entities\Pagination;
 trait PaginationCursorTrait
 {
     public ?PaginationCursor $paginationCursor;
+
+    public function getPaginationCursor(): ?PaginationCursor
+    {
+        if (!isset($this->paginationCursor)) {
+            $this->paginationCursor = new PaginationCursor();
+        }
+        return $this->paginationCursor;
+    }
 }
