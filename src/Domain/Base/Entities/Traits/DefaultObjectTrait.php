11e7f846515b37820f24077a1ed8295241a60bda
Marius-Eugen Gerdan <marius@gerdan.net>
2025-08-05 21:15:54 +0200

    Bump version to 2.8.8 and refactor `DefaultObjectTrait`: improve cloning logic, adjust `overwritePropertiesFromOtherObject` method, and correct `uniqueKey` implementation.

--- src/Domain/Base/Entities/Traits/DefaultObjectTrait.php
+++ src/Domain/Base/Entities/Traits/DefaultObjectTrait.php
@@ -88,11 +88,6 @@ trait DefaultObjectTrait
         return $selfJson == $otherJson;
     }
 
-    public function uniqueKey(): string
-    {
-        return self::uniqueKeyStatic(spl_object_id($this));
-    }
-
     /**
      * Verifies Identity by uniqueKey
      * @param BaseObject $other
@@ -108,6 +103,111 @@ trait DefaultObjectTrait
             return false;
         }
         return $this->uniqueKey() == $other->uniqueKey();
+    }
+
+    public function uniqueKey(): string
+    {
+        return self::uniqueKeyStatic(spl_object_id($this));
+    }
+
+    /**
+     * Overwerites all properties, that are set, from other object and maintains all properties of current object that are not set on other
+     *
+     * @param DefaultObject|null $other
+     * @param array $callPath
+     * @param bool $cloneProperties
+     *
+     * @return DefaultObject
+     * @throws ReflectionException
+     */
+    public function overwritePropertiesFromOtherObject(
+        ?DefaultObject $other,
+        array $callPath = [],
+        bool $cloneProperties = false
+    ): DefaultObject
+    {
+        if ($this === $other) {
+            return $this;
+        }
+        if (isset($callPath[spl_object_id($this)])) {
+            return $this;
+        }
+        // check identity of objects
+        if (!is_a($other, static::class)) {
+            return $this;
+        }
+        $callPath[spl_object_id($this)] = true;
+        $propertyNamesToIgnore = ['elements' => true];
+        // iterate through all public properties
+        foreach ($this->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
+            $propertyName = $property->getName();
+            if (isset($propertyNamesToIgnore[$propertyName])) {
+                // we ignore some properties, e.g. elements from ObjectSets
+                continue;
+            }
+            if (!isset($this->$propertyName) && isset($other->$propertyName)) {
+                // if a property is not set but set in $other, we use the one from $other
+                $otherProperty = $other->$propertyName;
+                if (is_object($otherProperty)) {
+                    if ($otherProperty instanceof DefaultObject) {
+                        // in case of DefaultObject, we use clone method
+                        if ($cloneProperties) {
+                            $otherProperty = $otherProperty->clone();
+                        }
+                    } else {
+                        // in default case, we use system clone method
+                        $otherProperty = $cloneProperties ? clone $otherProperty : $otherProperty;
+                    }
+                }
+                $this->$propertyName = $otherProperty;
+            } elseif (isset($this->$propertyName) && isset($other->$propertyName)) {
+                // in case of both $this and $other have property set
+                if ($other->$propertyName !== null) {
+                    $thisProperty = $this->$propertyName;
+                    $otherProperty = $other->$propertyName;
+                    if (is_object($otherProperty)) {
+                        // first check for identity of objects
+                        if ($thisProperty !== $otherProperty) {
+                            // in an object context
+                            if ($thisProperty instanceof DefaultObject) {
+                                // in case of DefaultObject, we overwrite recursively
+                                $thisProperty->overwritePropertiesFromOtherObject(
+                                    $otherProperty,
+                                    $callPath,
+                                    $cloneProperties
+                                );
+                            } else {
+                                $this->$propertyName = $cloneProperties ? clone $otherProperty : $otherProperty;
+                            }
+                        }
+                    } else {
+                        $this->$propertyName = $otherProperty;
+                    }
+                }
+            }
+        }
+        // handle ObjectSets
+        if ($this instanceof ObjectSet) {
+            /** @var ObjectSet $other */
+            foreach ($other->getElements() as $otherElement) {
+                $thisElement = $this->getByUniqueKey($otherElement->uniqueKey());
+                if ($otherElement instanceof DefaultObject) {
+                    if ($cloneProperties) {
+                        $otherElement = $otherElement->clone();
+                    }
+                } else {
+                    $otherElement = $cloneProperties ? clone $otherElement : $otherElement;
+                }
+                if (!$thisElement) {
+                    $this->add($otherElement);
+                } elseif (!($thisElement instanceof DefaultObject)) {
+                    $this->replace($otherElement);
+                } else {
+                    $thisElement->overwritePropertiesFromOtherObject($otherElement, $callPath, $cloneProperties);
+                }
+            }
+        }
+        return $this;
     }
 
     /**
@@ -195,98 +295,5 @@ trait DefaultObjectTrait
             }
         }
         return $clone;
-    }
-
-    /**
-     * Overwerites all properties, that are set, from other object and maintains all properties of current object that are not set on other
-     *
-     * @param DefaultObject|null $other
-     * @param array $callPath
-     * @param bool $cloneProperties
-     *
-     * @return DefaultObject
-     * @throws ReflectionException
-     */
-    public function overwritePropertiesFromOtherObject(
-        ?DefaultObject $other,
-        array $callPath = [],
-        bool $cloneProperties = false
-    ): DefaultObject {
-        if (isset($callPath[spl_object_id($this)])) {
-            return $this;
-        }
-        // check identity of objects
-        if (!is_a($other, static::class)) {
-            return $this;
-        }
-        $callPath[spl_object_id($this)] = true;
-        $propertyNamesToIgnore = ['elements' => true];
-        // iterate through all public properties
-        foreach ($this->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
-            $propertyName = $property->getName();
-            if (isset($propertyNamesToIgnore[$propertyName])) {
-                // we ignore some properties, e.g. elements from ObjectSets
-                continue;
-            }
-            if (!isset($this->$propertyName) && isset($other->$propertyName)) {
-                // if a property is not set but set in $other, we use the one from $other
-                $otherProperty = $other->$propertyName;
-                if (is_object($otherProperty)) {
-                    if ($otherProperty instanceof DefaultObject) {
-                        // in case of DefaultObject, we use clone method
-                        if ($cloneProperties) {
-                            $otherProperty = $otherProperty->clone();
-                        }
-                    } else {
-                        // in default case, we use system clone method
-                        $otherProperty = $cloneProperties ? clone $otherProperty : $otherProperty;
-                    }
-                }
-                $this->$propertyName = $otherProperty;
-            } elseif (isset($this->$propertyName) && isset($other->$propertyName)) {
-                // in case of both $this and $other have property set
-                if ($other->$propertyName !== null) {
-                    $thisProperty = $this->$propertyName;
-                    $otherProperty = $other->$propertyName;
-                    if (is_object($otherProperty)) {
-                        // in an object context
-                        if ($thisProperty instanceof DefaultObject) {
-                            // in case of DefaultObject, we overwrite recursively
-                            $thisProperty->overwritePropertiesFromOtherObject(
-                                $otherProperty,
-                                $callPath,
-                                $cloneProperties
-                            );
-                        } else {
-                            $this->$propertyName = $cloneProperties ? clone $otherProperty : $otherProperty;
-                        }
-                    } else {
-                        $this->$propertyName = $otherProperty;
-                    }
-                }
-            }
-        }
-        // handle ObjectSets
-        if ($this instanceof ObjectSet) {
-            /** @var ObjectSet $other */
-            foreach ($other->getElements() as $otherElement) {
-                $thisElement = $this->getByUniqueKey($otherElement->uniqueKey());
-                if ($otherElement instanceof DefaultObject) {
-                    if ($cloneProperties) {
-                        $otherElement = $otherElement->clone();
-                    }
-                } else {
-                    $otherElement = $cloneProperties ? clone $otherElement : $otherElement;
-                }
-                if (!$thisElement) {
-                    $this->add($otherElement);
-                } elseif (!($thisElement instanceof DefaultObject)) {
-                    $this->replace($otherElement);
-                } else {
-                    $thisElement->overwritePropertiesFromOtherObject($otherElement, $callPath, $cloneProperties);
-                }
-            }
-        }
-        return $this;
     }
 }
